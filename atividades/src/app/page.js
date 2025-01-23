30bb44f0afa6b6cae2fb1f729fc49568d1757fb5
anaholiveira <anagabrielyoliveira2@gmail.com>
2025-01-23 14:52:58 -0400

    atividade

--- atividades/src/app/page.js
+++ atividades/src/app/page.js
@@ -2,7 +2,7 @@
 import { useState } from 'react';
 import styles from './page.module.css';
 import Card from '@/components/Card';
-
+import Image from 'next/image';
 
 export default function Lista() {
     const paises = [
@@ -11,60 +11,70 @@ export default function Lista() {
             nome: 'Japão',
             continente: 'Ásia',
             populacao: '124 milhões ',
+            imagem: '/images/japao.png',
         },
         {
             id: 2,
             nome: 'Coreia do Sul ',
             continente: 'Ásia',
             populacao: '52 milhões',
+            imagem: '/images/coreia.png',
         },
         {
             id: 3,
             nome: 'Austrália',
             continente: 'Oceania',
             populacao: '26,5 milhões',
+            imagem: '/images/australia.jpg',
         },
         {
             id: 4,
             nome: 'França',
             continente: 'Europa',
             populacao: '67 milhões',
+            imagem: '/images/franca.png',
         },
         {
             id: 5,
             nome: 'Grécia',
             continente: 'Europa',
             populacao: '10,4 milhões',
+            imagem: '/images/grecia.png',
         },
         {
             id: 6,
             nome: 'Estados Unidos',
             continente: 'América do Norte',
             populacao: '334 milhões',
+            imagem: '/images/estadosunidos.jpg',
         },
         {
             id: 7,
             nome: 'Brasil',
             continente: 'América do Sul',
             populacao: '216 milhões',
+            imagem: '/images/brasil.jpg',
         },
         {
             id: 8,
             nome: 'Tailândia',
             continente: 'Ásia',
             populacao: '72 milhões',
+            imagem: '/images/tailandia.jpg',
         },
         {
             id: 9,
             nome: 'México',
             continente: 'América do Norte',
             populacao: '127 milhões',
+            imagem: '/images/mexico.jpeg',
         },
         {
             id: 10,
             nome: 'Espanha',
             continente: 'Europa',
             populacao: '47,7 milhões',
+            imagem: '/images/espanha.jpg',
         }
     ];
 
@@ -72,10 +82,10 @@ export default function Lista() {
     const paisesFiltrados = paises.filter(pais => pais.nome.toLowerCase().includes(busca.toLowerCase()));
 
     return (
-      <div className={styles.div}>
+      <div className={styles.divPrincipal}>
         <h1 className={styles.titulo}>˙⋆ Atividade ⋆˙</h1>
 
-        <div className={styles.inputDiv}>
+        <div className={styles.divInput}>
           <input
             className={styles.input}
             type = "text"
@@ -85,7 +95,7 @@ export default function Lista() {
           />
         </div>
 
-        <div className={styles.cardDiv}>
+        <div className={styles.divTotal}>
           {paisesFiltrados.map(pais => (
             <Card
               key={pais.id}
--- atividades/src/components/Card/index.js
+++ atividades/src/components/Card/index.js
@@ -1,12 +1,15 @@
 import styles from './Card.module.css';
+import Image from "next/image";
 
-    export default function Card({id, nome, continente, populacao}){
-    return (
-        <div className={styles.divCards}>
-            <div className={styles.cards}>
-                <h2 className={styles.idNome}>{id}{nome}</h2>
-                <p className={styles.continente}>{continente}</p>
-                <p className={styles.populacao}>{populacao}</p></div>
+export default function Card({id, nome, continente, populacao, imagem}){
+return (
+    <div>
+        <div className={styles.cards}>
+            <h2 className={styles.idNome}>{id}{nome}</h2>
+            <p className={styles.continente}>{continente}</p>
+            <p className={styles.populacao}>{populacao}</p>
+            <Image className={styles.imagens} src={imagem} alt={`Imagem de ${nome}`} width={235} height={135} />
         </div>
+    </div>
     )
 }
